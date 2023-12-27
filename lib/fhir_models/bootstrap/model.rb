cbf479a9e510fe295708d1260069d1d825051ab1
360dgries <dgriese@mitre.org>
2023-12-27 13:50:12 -0500

    Add guard against to_ary

--- lib/fhir_models/bootstrap/model.rb
+++ lib/fhir_models/bootstrap/model.rb
@@ -23,10 +23,10 @@ module FHIR
     end
 
     def respond_to_missing?(method_name, *)
-      defined?(self.class::MULTIPLE_TYPES) && self.class::MULTIPLE_TYPES[method_name.to_s] ||
-      !@extension.nil? && !@extension.empty? ||
-      !@modifierExtension.nil? && !@modifierExtension.empty? ||
-      super
+      (defined?(self.class::MULTIPLE_TYPES) && self.class::MULTIPLE_TYPES[method_name.to_s]) ||
+        (!@extension.nil? && !@extension.empty? && !@extension.respond_to?(:to_ary)) ||
+        (!@modifierExtension.nil? && !@modifierExtension.empty? && !@modifierExtension.respond_to?(:to_ary)) ||
+        super
     end
 
     # allow two FHIR models to be compared for equality
