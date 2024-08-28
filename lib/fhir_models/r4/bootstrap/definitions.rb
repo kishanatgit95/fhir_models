51e44a5f83b0013aa0e6d5c6f5de05ac924ec8b0
Yunwei Wang <>
2024-08-27 20:36:36 -0500

    Change FHIR::Definitions to FHIR::R4:Definitions

--- lib/fhir_models/r4/bootstrap/definitions.rb
+++ lib/fhir_models/r4/bootstrap/definitions.rb
@@ -1,16 +1,20 @@
 module FHIR
-  class Definitions
-    include CommonDefinitions
-    @ig_file_name = File.join(Dir.pwd, 'lib', 'fhir_models', 'igs', 'hl7.fhir.r4.core.tgz')
+  module R4
+    class Definitions
+      include CommonDefinitions
+      @ig_file_name = File.join(Dir.pwd, 'lib', 'fhir_models', 'igs', 'hl7.fhir.r4.core.tgz')
 
-    load_igs(@ig_file_name)
+      load_igs(@ig_file_name)
 
-    def self.module_version_name
-      'R4'
-    end
+      def self.module_version_name
+        'R4'
+      end
 
-    def self.module_version
-      FHIR::R4
+      def self.module_version
+        FHIR::R4
+      end
     end
   end
+
+  Definitions = FHIR::R4::Definitions
 end
