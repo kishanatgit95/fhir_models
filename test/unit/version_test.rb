7be70b52670c1e01a4611627f1b8b3ae5643bda4
360dgries <dgriese@mitre.org>
2024-09-23 17:40:54 -0400

    Fix VersionTest

--- test/unit/version_test.rb
+++ test/unit/version_test.rb
@@ -1,15 +1,15 @@
 require_relative '../test_helper'
 
 class VersionTest < Test::Unit::TestCase
-  json_file = File.read('lib/fhir_models/examples/json/allergyintolerance-nka.json')
-  xml_file = File.read('lib/fhir_models/examples/xml/allergyintolerance-nka.xml')
   def test_json_version
+    json_file = File.read('lib/fhir_models/examples/json/allergyintolerance-nka.json')
     assert_equal FHIR::Json.from_json(json_file).versioned_fhir_module, FHIR::R4
     assert_equal FHIR::R4::Json.from_json(json_file), FHIR::R4
     assert_equal FHIR::R4B::Json.from_json(json_file), FHIR::R4B
   end
 
   def test_xml_version
+    xml_file = File.read('lib/fhir_models/examples/xml/allergyintolerance-nka.xml')
     assert_equal FHIR::Xml.from_xml(xml_file).versioned_fhir_module, FHIR::R4
     assert_equal FHIR::R4::Xml.from_xml(xml_file), FHIR::R4
     assert_equal FHIR::R4B::Xml.from_xml(xml_file), FHIR::R4B
